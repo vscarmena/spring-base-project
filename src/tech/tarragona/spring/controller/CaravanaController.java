7232c483e4beede0c941b593d6ac867ac3e7266b
Marcos <lester.yeste@gmail.com>
2017-03-20 13:39:34 +0100

    AddCaravanaContrServ

--- src/tech/tarragona/spring/controller/CaravanaController.java
+++ src/tech/tarragona/spring/controller/CaravanaController.java
@@ -1,8 +1,38 @@
 package tech.tarragona.spring.controller;
 
+import javax.validation.Valid;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+import tech.tarragona.spring.model.Caravana;
+import tech.tarragona.spring.service.Caravanaservice;
+
+
 
 @Controller
+@RequestMapping(value = "/caravana")
 public class CaravanaController {
+	@Autowired
+	Caravanaservice caravanaService;
+	
+	@GetMapping("/caravana")
+	public String addPerson(Model model) {
+		model.addAttribute("person", new Caravana());
+		return "gestionCaravana";
+	}
+	@PostMapping("/caravana")
+	public String addCaravana(@Valid @ModelAttribute("caravana") Caravana caravana, BindingResult result, Model model){
+		caravanaService.addCaravana(caravana);
+		return "paginaDePruebas";
+	}
+	
+	
 
 }
