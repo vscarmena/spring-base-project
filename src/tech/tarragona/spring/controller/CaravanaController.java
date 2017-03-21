c0ddff328c5ac4e64010a843a3ae9d2e595f9e8c
Jordi Cebrián <jcebrian94@gmail.com>
2017-03-21 11:37:44 +0100

    Select people controller added

--- src/tech/tarragona/spring/controller/CaravanaController.java
+++ src/tech/tarragona/spring/controller/CaravanaController.java
@@ -1,8 +1,30 @@
 package tech.tarragona.spring.controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.PostMapping;
+
+import tech.tarragona.spring.model.Caravana;
+import tech.tarragona.spring.service.Caravanaservice;
 
 @Controller
 public class CaravanaController {
+	
+	@Autowired
+	Caravanaservice caravanaservice;
 
+	@PostMapping("/caravana")
+	public String seleccionarPersonas(Caravana caravana, BindingResult result, Model model)
+	{
+		int adults = caravana.getAdults();
+		int kids = caravana.getKids();
+		int babys = caravana.getBabys();
+		
+		caravanaservice.seleccionarPersonas(adults, babys, kids, caravana);
+		
+		return "gestionCaravana";
+	}
+	
 }
--- src/tech/tarragona/spring/service/Caravanaservice.java
+++ src/tech/tarragona/spring/service/Caravanaservice.java
@@ -1,8 +1,24 @@
 package tech.tarragona.spring.service;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
+
+import tech.tarragona.spring.model.Caravana;
+import tech.tarragona.spring.repository.CaravanaRepository;
 
 @Service
 public class Caravanaservice {
+	
+	@Autowired
+	CaravanaRepository caravanaRepository;
 
+	public void seleccionarPersonas(int adults, int babys, int kids, Caravana caravana)
+	{
+		caravana.setAdults(adults);
+		caravana.setBabys(babys);
+		caravana.setKids(kids);
+		
+		caravanaRepository.save(caravana);
+	}
+	
 }
