639ef2754d590be1ffcd706b4db693a8e0161aba
jaume <jaume111994@hotmail.com>
2017-03-22 09:28:34 +0100

    login jsp

--- src/tech/tarragona/spring/controller/MainController.java
+++ src/tech/tarragona/spring/controller/MainController.java
@@ -39,6 +39,11 @@ public class MainController {
 	  }
 	  
 	  @RequestMapping("/")
+	  public String start(Model model) {
+	      return "hello";
+	  }
+	  
+	  @RequestMapping("/hello")
 	  public String hello(Model model) {
 	      return "hello";
 	  }
