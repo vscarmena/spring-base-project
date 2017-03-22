5320413143fbe3251f31004da7b8a241a4b4cd12
Xavi <crafttutos@gmail.com>
2017-03-22 12:49:24 +0100

    main/java/ PATH FIXED

--- src/main/java/tech/tarragona/spring/config/SecurityConfig.java
+++ src/main/java/tech/tarragona/spring/config/SecurityConfig.java
@@ -22,7 +22,7 @@ public class SecurityConfig extends WebSecurityConfigurerAdapter {
     protected void configure(HttpSecurity http) throws Exception {
 		http
 			.authorizeRequests()
-		        .antMatchers("/").permitAll()
+		        .antMatchers("/**").permitAll()
 		        .anyRequest().authenticated()
 		        .and()
 		    .formLogin()
